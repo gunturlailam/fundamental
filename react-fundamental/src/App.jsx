a2155c19e05a8d8e69364402ac35c0de70773585
Guntur Lailam Yuro <gunturlailamyuro8@gmail.com>
2025-10-11 07:20:55 +0700

    chore(learn): latihan membuat komponen React

--- react-fundamental/src/App.jsx
+++ react-fundamental/src/App.jsx
@@ -1,50 +1,41 @@
-// jsx dasar
+// functional component
+
+// function Welcome(){
+//   return <h1>Halo, ini adalah component Welcome!</h1>
+// }
 
 // function App(){
-//   return(
+//   return (
 //     <div>
-//       <h1>Hello React</h1>
-//       <p>Belajar JSX itu menyenangkan!</p>
+//       <Welcome/>
+//       {/* reusable component */}
+//       <Welcome/>
+//       <Welcome/>
 //     </div>
 //   )
 // }
 
-// export default App
+// export default App;
 
 
 
-// ekspresi javascript di JSX
-
-// const name = "Guntur Lailam Yuro"
-
-// function App(){
-//   return(
-//     <div>
-//       <h1>Halo, {name}!</h1>
-//       <p>Saya semangat belajar react!</p>
-//     </div>
-//   )
-// }
-
-// export default App
+// harus diawali huruf besar
+// function Welcome(){
+//   return <h1>Hello</h1>
+// } 
 
 
-// aturan dasar jsx
-// 1 . hanya boleh satu parent elemen
-// function App(){
-//   return(
-//   <div>
-//     <h1>Hello</h1>
-//     <p>World</p>
-//   </div>
-// );
-// }
+
+// moduldarisasi komponent
+import Header from "./components/Header";
+
+function App(){
+  return(
+    <div>
+      <Header/>
+      <p>Halo,ini halaman utama React</p>
+    </div>
+  )
+}
 
 export default App
-
-// 2. gunakan className bukan class
-{/* <h1 className="title">Hello React</h1> */}
-
-
-// 3. gunakan camelCase
-{/* <button onClick={() => alert("Clicked!")}>Klik Saya</button> */}
