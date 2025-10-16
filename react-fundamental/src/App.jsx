969d3a94595669ebeed2af299bf49f250acd05a8
Guntur Lailam Yuro <gunturlailamyuro8@gmail.com>
2025-10-16 10:43:21 +0700

    learn: belajar form di React

--- react-fundamental/src/App.jsx
+++ react-fundamental/src/App.jsx
@@ -1,100 +1,83 @@
-// list menggunakan map()
-// function App(){
-//   const names = ["Budi","Siti","Andi","Dewi"]
+// controlled component
+// import { useState } from "react";
 
-//   return(
-//     <div>
-//       <h1>Daftar Nama:</h1>
-//       <ul>
-//         {
-//           names.map((name) => (
-//             <li>{name}</li>
-//           ))
-//         }
-//       </ul>
-//     </div>
-//   )
+// function App(){
+//     const [name, setName] = useState("")
+
+//     const handleSubmit = (event) => {
+//         event.preventDefault();
+//         alert(`Nama:${name}`)
+//     }
+
+//     return(
+//         <form onSubmit={handleSubmit}>
+//             <input 
+//             type="text"
+//             value={name}
+//             onChange={(e) => setName(e.target.value)}
+//             placeholder="Masukkan Nama" />
+//             <button type="submit">Kirim</button>
+//         </form>
+//     )
 // }
 
 // export default App
 
 
-// key pada list
-// function App(){
-//   const names = ["Budi","Siti","Andi","Dewi "]
+// uncontrolled component
+// import { useRef } from "react";
 
-//   return(
-//     <div>
-//       <h1>Daftar Nama:</h1>
-//       <ul>
-//         {
-//           names.map((name,index) => (
-//             <li key={index}>{name}</li>
-//           ))
-//         }
-//       </ul>
-//     </div>
-//   )
+// function App(){
+//     const nameRef = useRef()
+
+//     const handleSubmit = (event) => {
+//         event.preventDefault()
+//         alert(`Nama: ${nameRef.current.value}`)
+//     }
+
+//     return(
+//         <form onSubmit={handleSubmit}>
+//             <input type="text" ref={nameRef} placeholder="Masukkan Nama" />
+//             <button type="submit">Kirim</button>
+//         </form>
+//     )
 // }
 
 // export default App
 
 
-// list dengan object
-// function App(){
-//   const users = [
-//     {id:1,name:"Budi",age:20},
-//     {id:2,name:"Siti",age:22},
-//     {id:3,name:"Andi",age:19},
-//   ]
-
-//   return(
-//     <div>
-//       <h1>Daftar User:</h1>
-//       <ul>
-//         {
-//           users.map((user) => (
-//             <li key={user.id}>
-//               {user.name} - {user.age} tahun
-//             </li>
-//           ))
-//         }
-//       </ul>
-//     </div>
-//   )
-// }
-
-// export default App
-
-
-// list dengan komponent terpisah
-function UserItem({name,age}){
-  return(
-    <li>
-      {name} - {age} tahun
-    </li>
-  )
-}
+// form dengan beberapa input
+import { useState } from "react";
 
 function App(){
-  const users = [
-    {id:1,name:"Budi",age:20},
-    {id:2,name:"Siti",age:22},
-    {id:3,name:"Andi",age:19}
-  ]
+    const [form, setForm] = useState({email:"",password: ""});
 
-  return(
-    <div>
-      <h1>Daftar User:</h1>
-      <ul>
-        {
-          users.map((user) => (
-            <UserItem key={user.id} name={user.name} age={user.age}/>
-          ))
-        }
-      </ul>
-    </div>
-  )
+    const handleChange = (e) => {
+        setForm({...form, [e.target.name]: e.target.value})
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        alert( `Email:  ${form.email}, Password: ${form.password}`)
+    }
+
+    return(
+        <form onSubmit={handleSubmit}>
+            <input 
+            type="email"
+            name="email"
+            placeholder="Masukkan Email"
+            value={form.email}
+            onChange={handleChange} />
+            <input 
+            type="password"
+            name="password"
+            placeholder="Masukkan Password" 
+            value={form.password}
+            onChange={handleChange}/>
+            <button type="submit">Login</button>
+        </form>
+    )
 }
 
 export default App
