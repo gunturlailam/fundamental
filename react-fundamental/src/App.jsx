c09ffa70e9a6e6a5d4c4819e01bc68abcbca9960
Guntur Lailam Yuro <gunturlailamyuro8@gmail.com>
2025-10-18 09:44:30 +0700

    learn: belajar styles di React

--- react-fundamental/src/App.jsx
+++ react-fundamental/src/App.jsx
@@ -1,53 +1,45 @@
-// CUSTOM HOOK SEDERHANA
-// import useWindowWidth from "./hooks/useWindowWidth";
+// CSS Modules
+import styles from "./App.module.css";
 
+function App() {
+  return (
+    <div>
+      <h1 className={styles.title}>
+        Halo, ini menggunakan CSS Modules (hijau)
+      </h1>
+    </div>
+  );
+}
+
+export default App;
+
+// Contoh lainnya (uncomment untuk mencoba):
+
+// Inline style:
 // function App(){
-//   const width = useWindowWidth()
-
-//   return(
+//   return (
 //     <div>
-//       <h1>Lebar Jendela: {width}px</h1>
+//       <h1 style={{ color:"blue", fontSize: "24px"}}>
+//         Halo, ini teks berwarna biru
+//       </h1>
 //     </div>
 //   )
 // }
 
-// export default App
+// CSS eksternal:
+// import "./App.css"
+// function App(){
+//   return <h1 className="title">
+//     Halo, ini teks berwarna merah
+//   </h1>
+// }
 
-
-// CUSTOM HOOK AGAK KOMPLEKS
-import useForm from "./hooks/useForm";
-
-function App(){
-  const {values, handleChange, resetForm} = useForm({
-    email: "",
-    password:""
-  })
-
-  const handleSubmit = (e) => {
-    e.preventDefault()
-    alert(`Email: ${values.email}, Password"${values.password}`)
-    resetForm()
-  } 
-
-  return(
-    <form onSubmit={handleSubmit}>
-      <input 
-      type="email"
-      name="email"
-      value={values.email}
-      onChange={handleChange}
-      placeholder="Email"
-      />
-      <input 
-      type="password"
-      name="password"
-      value={values.password}
-      onChange={handleChange}
-      placeholder="Password" />
-      
-      <button type="submit">Login</button>
-    </form>
-  )
-}
-
-export default App
+// Styled Components (perlu install dulu: npm install styled-components):
+// import styled from "styled-components";
+// const Title = styled.h1`
+//   color: purple;
+//   text-decoration: underline;
+// `;
+// function App() {
+//   return <Title>Halo, ini teks berwarna ungu</Title>;
+// }
--- react-fundamental/src/components/Header.jsx
+++
@@ -1,5 +0,0 @@
-function Header(){
-    return <h1>Ini adalah Header!</h1>
-}
-
-export default Header
