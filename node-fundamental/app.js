b9ca14235c87d0debf5decdf6b2dd19da3203c12
Guntur Lailam Yuro <gunturlailamyuro8@gmail.com>
2025-10-31 06:28:29 +0700

    learn: Belajar global object

--- node-fundamental/app.js
+++ node-fundamental/app.js
@@ -1,1 +1,19 @@
-console.log("Halo, ini adalah file Javascipt pertama kita di node.js!")
+// 1. Global 
+// global.nama = "NodeJS";
+
+// console.log("Halo " + global.nama); // Halo NodeJS
+
+// 2. Console
+// console.log("Halo dari nodejs"); // Halo dari nodejs
+
+// 3. Process
+// console.log(process.platform); 
+
+// 4 setTimeout
+// setTimeout(() => {
+//     console.log("Hello after 2 seconds");
+// },2000);
+
+// 5.Buffer
+const buf = Buffer.from("Hello NodeJS");
+console.log(buf);
